279a6a68338093db417ac1f31417dbfd7aa9ec1c
Abiodun Awoyemi <aabiodunawoyemi@gmail.com>
2021-09-16 22:42:11 +0100

    solution to two sum

---
+++ 1-TwoSum/index.js
@@ -0,0 +1,12 @@
+const twoSum = (nums, target) => {
+    const arr = [];
+    for (i = 0; i <= nums.length; i++) {
+        for(j = i + 1; j <= nums.length; j++) {
+            if(nums[i] + nums[j] === target ) {
+                arr.push(i)
+                arr.push(j)
+            }
+        }
+    }
+    return arr
+};
---
+++ README.md
@@ -0,0 +1,3 @@
+# leetcode-solution
+
+This repository contains the solution for leetcode problems.
